edecf6b8b4060d292526254c399124319543e698
Henry <hhhguany@me.com>
2019-03-09 14:00:42 +0800

    update  todo

---
+++ math.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+class GF2nField:
+    __n=0
+
+    def __init__(self,n):
+        isinstance(n,int)
+        self.__n=n
+    
+    def 
+
+
+
+def if __name__ == "__main__":
+    for 
